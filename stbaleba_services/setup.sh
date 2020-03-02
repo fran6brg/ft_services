d79f1a561114bf53c364e636257e0a7aa5726428
Steve-alan BALEBA BALEBA <stbaleba@e1r11p21.42.fr>
2020-03-02 14:06:36 +0100

    add my work

--- stbaleba_services/setup.sh
+++ stbaleba_services/setup.sh
@@ -1,8 +1,14 @@
 export MINIKUBE_HOME=~/goinfre
 minikube config set vm-driver virtualbox
 minikube start
+minikube docker-env
+eval $(minikube -p minikube docker-env)
 bash srcs/ssl.sh
-kubectl create -f /Users/stbaleba/42_Circle_3/github_service/srcs/ingress.yml
+#bash srcs/docker_alpine/ftps_ssl.sh
+kubectl create -f srcs/ingress.yml
 minikube addons enable ingress
 kubectl apply -f srcs/service-nginx.yml
+docker build -t test_alpine ./srcs/docker_alpine/
+kubectl apply -f srcs/ftps.yml
+#rm srcs/docker_alpine/vsftpd.pem
 minikube dashboard
