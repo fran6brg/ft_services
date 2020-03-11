4e00de063b02599c78147d23bde43707f1a73707
Harthann <nca.eyraud@gmail.com>
2020-03-11 22:53:10 +0100

    add: html nginx page

--- nieyraud_services/v7/srcs/nginx/entrypoint.sh
+++ nieyraud_services/v7/srcs/nginx/entrypoint.sh
@@ -1,4 +1,6 @@
 php-fpm7
+mv /index.nginx-debian.html /usr/share/nginx/html/.
+
 echo "fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;" >> /etc/nginx/fastcgi_params
 chown -R nginx:nginx /etc/php7
 chown -R nginx:nginx /usr/share/nginx/html
