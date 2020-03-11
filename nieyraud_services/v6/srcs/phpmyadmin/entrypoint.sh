b57fe288df76e8af093c0838354113bbc6e23c17
Harthann <nca.eyraud@gmail.com>
2020-03-11 05:14:39 +0100

    add: v7 starting, abort unique nginx project

--- nieyraud_services/v6/srcs/phpmyadmin/entrypoint.sh
+++ nieyraud_services/v6/srcs/phpmyadmin/entrypoint.sh
@@ -1,3 +1,8 @@
-mv phpMyAdmin-4.9.4-all-languages /usr/share/nginx/html/phpmyadmin
+# mv phpMyAdmin-4.9.4-all-languages /usr/share/nginx/html/phpmyadmin
+ln -s phpMyAdmin-4.9.4-all-languages /usr/share/nginx/html/phpmyadmin
 mysql -h $PMA_HOST -u $MYSQL_USER -p$MYSQL_ROOT_PASSWORD -e 'USE wordpress'
+while  [ $? == 1 ]; do
+	sleep 2;
+	mysql -h $PMA_HOST -u $MYSQL_USER -p$MYSQL_ROOT_PASSWORD -e 'USE wordpress'
+done
 php-fpm7 -F
