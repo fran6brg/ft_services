ede0e7805737c0843c325e1b0d5327444f5438f5
Harthann <nca.eyraud@gmail.com>
2020-07-28 11:46:58 +0200

    starting to build my own ftps dockerfile

--- nieyraud_services/v7/srcs/ftps/entrypoint.sh
+++ nieyraud_services/v7/srcs/ftps/entrypoint.sh
@@ -1,4 +1,2 @@
-#! bin/sh
-
-#nohup touch /run/openrc/softlevel && rc-service vsftpd restart &
-touch /run/openrc/softlevel && rc-service vsftpd restart && tail -f /dev/null
+echo "root:root" | chpasswd
+vsftpd
